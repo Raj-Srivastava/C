260dbba3b814447331c4838edbe20d2a6354fc99
Raj-Srivastava <77150093+Raj-Srivastava@users.noreply.github.com>
2023-01-06 22:42:49 +0530

    Create 1darray.c

---
+++ 1darray.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int arr[1000],i,num,sum=0;
+    scanf("%d",&num);
+    for(i=0;i<num;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<num;i++)
+    {
+        sum+=arr[i];
+    }
+    printf("%d",sum);
+    return 0;
+}
